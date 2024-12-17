92d0e8b9e7dbc3c5d7a952bebf12fab334d0c5b4
hengle92 <79264847+hengle92@users.noreply.github.com>
2024-12-17 13:51:44 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,42 @@
-self.addEventListener('install', function(event) {
-  console.log('Service Worker 安装成功');
+const CACHE_NAME = "esp-control-v1";
+const CACHE_FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icon.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open('esp-control-cache').then(function(cache) {
-      return cache.addAll([
-        './',
-        './index.html',
-        './style.css',
-        './script.js',
-        './manifest.json',
-        './icon.png'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Service Worker: 缓存文件...");
+      return cache.addAll(CACHE_FILES);
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log("Service Worker: 清理旧缓存", key);
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
 });
